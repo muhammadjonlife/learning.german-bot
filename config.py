e980aaad69beb79be6e30ced627831018d83a99b
muhammadjonlife <zoirovmukhammadjon@gmail.com>
2026-05-03 21:38:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = "8644111630:AAHysBQdaHHin_-TtMHSrOmmaGDQwJo3oP4"
-OPENAI_API_KEY = ""
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 MODEL = "gpt-4o-mini"
